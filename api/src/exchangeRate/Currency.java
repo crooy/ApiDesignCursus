17a313ed2753a25208804cbef6c1c99f57a7e184
Ronald Crooy <r.a.crooy@gmail.com>
2011-11-30 12:32:25 +0100

    added gitignore and code for currency

--- api/src/exchangeRate/Currency.java
+++ api/src/exchangeRate/Currency.java
@@ -8,6 +8,37 @@ package exchangeRate;
  *
  * @author ronald
  */
-public class Currency {
+public final class Currency {
+    
+    private final String name;
+    private final String sign;
+    
+    public Currency(String name, String sign){
+        this.name = name;
+        this.sign = sign;
+    }
+    
+    public final String Name(){
+        return name;
+    }
+    
+    public final String Sign(){
+        return sign;
+    }
+    
+    public final String toString(){
+        return Sign();
+    }
+    
+    public boolean equals(Object other){
+        if (other == null) return false;
+        
+        boolean sameClass = other.getClass().equals(this.getClass());
+        if (!sameClass) return false;
+        
+        Currency c = (Currency)other;
+        return ( c.name.equals(this.name));
+    }
+   
     
 }
