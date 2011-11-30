ed84a13a3cbd441d09a9b715e8649dea10e8c9b5
Frank Kusters <frank@frank-Latitude-D610.(none)>
2011-11-30 13:25:40 +0100

    commit calculator with exceptions

--- api/src/exchangeRate/Calculator.java
+++ api/src/exchangeRate/Calculator.java
@@ -11,17 +11,41 @@ package exchangeRate;
 public class Calculator {
     private ExchangeRate exchangeRate;
     
-    public CurrencyValue convert(CurrencyValue from, Currency to) {
-        // find the conversion direction
+    public CurrencyValue convert(CurrencyValue from, Currency to) throws CalculatorException {
+        assert (!exchangeRate.getFirstCurrency().equals(exchangeRate.getSecondCurrency()));
+        
+        boolean directionEqualToExchangeRate =
+                                getExchangeDirection(to);
+        
+        if (directionEqualToExchangeRate)
+        {
+            if (! exchangeRate.getFirstCurrency().equals(from)) {
+                throw new CalculatorException("the 'from'-currency is not valid");
+            }
+        }
+        else {
+            if (! exchangeRate.getSecondCurrency().equals(from)) {
+                throw new CalculatorException("the 'from'-currency is not valid");
+            }
+        }
+        
+        return new CurrencyValue(to, 0.0);
+    }
+
+    private boolean getExchangeDirection(Currency to) throws CalculatorException {
+        boolean directionEqualToExchangeRate;
+        
         if (exchangeRate.getSecondCurrency().equals(to))
         {
-            // same direction as ExchangeRate
+            directionEqualToExchangeRate = true;
         }
         else if (exchangeRate.getFirstCurrency.equals(to)) {
-            // other direction
+            directionEqualToExchangeRate = false;
         }
         else {
             throw new CalculatorException("the 'to'-currency is not valid");
         }
+        
+        return directionEqualToExchangeRate;
     }
 }
