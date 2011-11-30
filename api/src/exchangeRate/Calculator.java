89a99c8bd5c9f1511e805cc2a205b571b27799af
Frank Kusters <frank@frank-Latitude-D610.(none)>
2011-11-30 13:38:27 +0100

    finalize convert()

--- api/src/exchangeRate/Calculator.java
+++ api/src/exchangeRate/Calculator.java
@@ -14,9 +14,28 @@ public class Calculator {
     public CurrencyValue convert(CurrencyValue from, Currency to) throws CalculatorException {
         assert (!exchangeRate.getFirstCurrency().equals(exchangeRate.getSecondCurrency()));
         
-        boolean directionEqualToExchangeRate =
-                                getExchangeDirection(to);
+        boolean directionEqualToExchangeRate = getExchangeDirection(to);
+        verifyCurrencyFrom(directionEqualToExchangeRate, from);
         
+        // we're set, lets convert!
+        double newValue;
+        if (directionEqualToExchangeRate) {
+            newValue = from.getValue() / exchangeRate.getExchangeRate();
+        }
+        else {
+            newValue = from.getValue() * exchangeRate.getExchangeRate();
+        }
+        
+        return new CurrencyValue(to, newValue);
+    }
+
+    /**
+     * Verifies that the 'from' currency is a valid currency.
+     * @param directionEqualToExchangeRate
+     * @param from
+     * @throws CalculatorException 
+     */
+    private void verifyCurrencyFrom(boolean directionEqualToExchangeRate, CurrencyValue from) throws CalculatorException {
         if (directionEqualToExchangeRate)
         {
             if (! exchangeRate.getFirstCurrency().equals(from)) {
@@ -28,24 +47,30 @@ public class Calculator {
                 throw new CalculatorException("the 'from'-currency is not valid");
             }
         }
-        
-        return new CurrencyValue(to, 0.0);
     }
 
+    /**
+     * Find out if the 'direction' of the exchange is equal to the direction
+     * of the exchangeRate member. The direction for the exchange is:
+     * from --> to
+     * And for the exchangeRate:
+     * FirstCurrency --> SecondCurrency
+     * @param to
+     * @return true if the direction is equal, false if it is not
+     * @throws CalculatorException 
+     */
     private boolean getExchangeDirection(Currency to) throws CalculatorException {
         boolean directionEqualToExchangeRate;
         
         if (exchangeRate.getSecondCurrency().equals(to))
         {
-            directionEqualToExchangeRate = true;
+            return true;
         }
         else if (exchangeRate.getFirstCurrency().equals(to)) {
-            directionEqualToExchangeRate = false;
+            return false;
         }
         else {
             throw new CalculatorException("the 'to'-currency is not valid");
         }
-        
-        return directionEqualToExchangeRate;
     }
 }
