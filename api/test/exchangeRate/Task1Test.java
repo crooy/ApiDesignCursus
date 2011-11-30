928ae3af099fb41a2872f0f6348b9b861dbaafff
Ronald Crooy <r.a.crooy@gmail.com>
2011-11-30 11:54:58 +0100

    tests

---
+++ api/test/exchangeRate/Task1Test.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exchangeRate;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ *
+ * @author ronald
+ */
+public class Task1Test {
+    
+    public Task1Test() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
