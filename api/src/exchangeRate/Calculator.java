c2f33256b755b8005f6f944b1e181a65bf12756c
Frank Kusters <frank@frank-Latitude-D610.(none)>
2011-11-30 13:08:23 +0100

    partial convert() implementation

--- api/src/exchangeRate/Calculator.java
+++ api/src/exchangeRate/Calculator.java
@@ -9,5 +9,19 @@ package exchangeRate;
  * @author ronald
  */
 public class Calculator {
+    private ExchangeRate exchangeRate;
     
+    public CurrencyValue convert(CurrencyValue from, Currency to) {
+        // find the conversion direction
+        if (exchangeRate.getSecondCurrency().equals(to))
+        {
+            // same direction as ExchangeRate
+        }
+        else if (exchangeRate.getFirstCurrency.equals(to)) {
+            // other direction
+        }
+        else {
+            throw new CalculatorException("the 'to'-currency is not valid");
+        }
+    }
 }
