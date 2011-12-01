373414d149d0a04e01dc8d2ca9b901934dbe32e6
Ronald Crooy <r.a.crooy@gmail.com>
2011-12-01 11:45:28 +0100

    fixes test removed mocking

--- api/test/exchangeRate/CalculatorFactoryTest.java
+++ api/test/exchangeRate/CalculatorFactoryTest.java
@@ -7,11 +7,10 @@ package exchangeRate;
 import java.util.Set;
 import java.util.HashSet;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mockito.Mockito;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+
 
 /**
  *
@@ -22,9 +21,9 @@ public class CalculatorFactoryTest {
     
     
     
-    @Test
+    @Test(expected = Exception.class) 
     public void testMergeEmptyExchangeRates() throws ExchangeRateCalculatorException{
-        ExchangeRates mockRates = mock(ExchangeRates.class);
+        ExchangeRates mockRates = new ExchangeRates();
         
         CalculatorFactory tested = new CalculatorFactory(mockRates);
         
@@ -32,24 +31,24 @@ public class CalculatorFactoryTest {
         Set<ExchangeRate> second = new HashSet<ExchangeRate>();
         
         tested.mergeExchangeRates(first, second);
-        fail("should not be able to work with empty sets");
+        Assert.fail("should not be able to work with empty sets");
     }
     
-    @Test
+    @Test(expected = Exception.class)
     public void testMergeNullExchangeRates() throws ExchangeRateCalculatorException{
         
-        ExchangeRates mockRates = mock(ExchangeRates.class);
+        ExchangeRates mockRates = new ExchangeRates();
         
         CalculatorFactory tested = new CalculatorFactory(mockRates);
         
         tested.mergeExchangeRates(null, null);
-        fail("should not be able to work with null sets");
+        Assert.fail("should not be able to work with null sets");
     }
      
-    @Test
+    @Test(expected = Exception.class)
     public void testMergeEqualExchangeRates() throws ExchangeRateCalculatorException{
         
-        ExchangeRates mockRates = mock(ExchangeRates.class);
+        ExchangeRates mockRates = new ExchangeRates();
         
         CalculatorFactory tested = new CalculatorFactory(mockRates);
         
@@ -57,6 +56,6 @@ public class CalculatorFactoryTest {
         
         tested.mergeExchangeRates(first, first);
         
-        fail("should not be able to work with equal sets");
+        Assert.fail("should not be able to work with equal sets");
     }
 }
