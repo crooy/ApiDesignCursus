7ee52aea5c4c7253dc1da5d35619ed2793daf54c
Ronald Crooy <r.a.crooy@gmail.com>
2011-12-02 10:14:55 +0100

    added breaks for group 3

---
+++ andere_groepen/groep3/taak2/Calculator/test/test/DiffTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import exchangeRate.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ronald
+ */
+public class DiffTest {
+    @Test(expected = NullPointerException.class)
+    public void testDifferentException() throws Exception{
+        Calculator c = null;
+        
+        c = CalculatorImpl.create(Currencies.eUSD, null, 17);
+    }
+    
+}
